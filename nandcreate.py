8ac79fb745fb8ff3d7a091820812b7dd3ff22f45
biohack5079 <biohack5079@gmail.com>
2024-08-08 06:00:56 +0900

    Update nandcreate.py

--- nandcreate.py
+++ nandcreate.py
@@ -1,25 +1,79 @@
-# パーセプトロンのNAND回路からすべての論理回路をつくる  
+# シグモイドパーセプトロン
 
 import numpy as np
 
 x = int(input('１か０を入力してください x: '))
 y = int(input('１か０を入力してください y: '))
+r = np.array([x, y])
+w = np.array([1, 1])
+s = -1.5 #threshold
+m = np.dot(w,r) + s
 
-def NAND(x, y):
+print('単純パーセプトロンの値')
+print(m)
+
+# シグモイド関数
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x)) -0.5
+
+print('シグモイドパーセプトロンの値')
+print(sigmoid(m))
+  
+
+def AND(x, y):
     r = np.array([x, y])
-    w = np.array([-1, -1])
+    w = np.array([1, 1])
     s = -1.5 #threshold
-    if np.sum(w * r) - s > 0:
+    m = np.dot(w,r) + s
+        
+    if sigmoid(m) > 0:
         z = 1
     else:
         z = 0
     return z
     
-"""  
-デフォルトの論理回路で書くと
-def nand(x, y):
-    return not (x and y)
-"""
+def OR(x, y):
+    r = np.array([x, y])
+    w = np.array([1, 1])
+    s = -0.5 #threshold
+    m = np.dot(w,r) + s
+        
+    if sigmoid(m) > 0:
+        z = 1
+    else:
+        z = 0
+    return z
+
+def NAND(x, y):
+    r = np.array([x, y])
+    w = np.array([-1, -1])
+    s = 1.5 #threshold
+    m = np.dot(w,r) + s
+        
+    if sigmoid(m) > 0:
+        z = 1
+    else:
+        z = 0
+    return z
+
+def NOR(x, y):
+    r = np.array([x, y])
+    w = np.array([-1, -1])
+    s = 0.5 #threshold
+    m = np.dot(w,r) + s
+        
+    if sigmoid(m) > 0:
+        z = 1
+    else:
+        z = 0
+    return z
+
+print('単層')
+print('AND=' + str(AND(x, y))) # AND
+print('OR=' + str(OR(x, y))) # OR
+print('NAND=' + str(NAND(x, y))) # NAND
+print('NOR=' + str(NOR(x, y))) # NOR
+
 
 def AND(x, y):
     return NAND(NAND(x, y), NAND(x, y))
@@ -31,8 +85,11 @@ def XOR(x, y):
     return NAND(NAND(NAND(x, y),y), NAND(x,NAND(x, y)))
     
 def NOR(x, y):
-    return NAND(NAND(NAND(x, y), NAND(x, y)), NAND(NAND(x, y), NAND(x, y)))
+    return NAND( NAND(NAND(x, x), NAND(y, y)), NAND(NAND(x, x), NAND(y, y)) )
 
+
+
+print('多層')
 print('AND=' + str(AND(x, y))) # AND
 print('OR=' + str(OR(x, y))) # OR
 print('NAND=' + str(NAND(x, y))) # NAND
