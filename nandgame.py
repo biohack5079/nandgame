d687989be6d4f62c35c9378bc8e49f55a6b1e72b
biohack5079 <biohack5079@gmail.com>
2024-09-15 04:33:38 +0900

    fi

---
+++ nandgame.py
@@ -0,0 +1,28 @@
+import tkinter
+
+root = tkinter.Tk()
+root.title("NANDGAME")
+root.minsize(800, 450)
+top = tkinter.Label(root, text="\nNANDGAME\n", font=("メイリオ", 16, "bold"))
+top.pack()
+top = tkinter.Label(root, text="1か0を入力してください", font=("メイリオ", 12, "bold"))
+top.pack()
+
+e1 = tkinter.Entry(root)
+e1.pack()
+e2 = tkinter.Entry(root)
+e2.pack()
+button = tkinter.Button(root, text="１か0を入力")
+button.pack()
+
+
+def nandgame():
+    x = int(e1.get())
+    y = int(e2.get())
+    display = "\nAND=" + str(x*y) + "です\n""NAND=" + str(1-x*y) + "です\n""OR=" + str(x+y-x*y) + "です\n""NOR=" + str(1-x-y+x*y) + "です\n""XOR=" + str(x+y-2*x*y) + "です\n""XNOR=" + str(1-x-y+2*x*y) + "です\n"
+    top["text"] = display
+
+
+
+button["command"] = nandgame
+root.mainloop()
